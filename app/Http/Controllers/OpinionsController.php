fe9a916e7037ff2b6dbcc8643915225469d0776e
RK <rezaul.cse.pust17@gmail.com>
2022-03-04 18:15:17 +0600

    Inserting data by OpinionController

--- app/Http/Controllers/OpinionsController.php
+++ app/Http/Controllers/OpinionsController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Opinion;
+use Illuminate\Support\Facades\DB;
 
 class OpinionsController extends Controller
 {
@@ -15,6 +17,22 @@ class OpinionsController extends Controller
 
     public function OpinionStore(Request $request)
     {
-       print_r($request->input());
+      
+      // print_r($request->input());
+
+        $data = array(
+
+            'fullname' => $request->fullname,
+            'mobile_no' => $request->mobileno,
+            'email' => $request->email,
+            'address' => $request->address,
+            'comments' => $request->subject
+            
+        );
+
+        DB::table('opinions')->insert($data);
+        return redirect()->back();
+
+
     }
 }
