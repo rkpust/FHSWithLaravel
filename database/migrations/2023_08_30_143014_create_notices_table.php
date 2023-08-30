86312ec96830faa8053409e607a3fc70532dc4bd
rkpust <rezaul.cse.pust17@gmail.com>
2023-08-30 20:52:23 +0600

    added notices table

---
+++ database/migrations/2023_08_30_143014_create_notices_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNoticesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notices', function (Blueprint $table) {
+            $table->id();
+            $table->date('PubDate');
+            $table->string('title');
+            $table->string('fileLink');
+            $table->string('AdminEmail');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notices');
+    }
+}
